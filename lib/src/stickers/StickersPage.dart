8e46c618c9729508cc6e31bb9f7ca1e8a3ff58e7
sukhbeer <sukhbeer.maurya@brainmobi.com>
2019-04-18 11:27:57 +0530

    ios_compilation_fixed

--- lib/src/stickers/StickersPage.dart
+++ lib/src/stickers/StickersPage.dart
@@ -1,28 +1,21 @@
-import 'dart:async';
-import 'dart:typed_data';
-import 'dart:ui' as ui;
-
 import 'package:flutter/material.dart';
-import 'package:flutter_appasd/src/stickers/AnimModel.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
-
 class StickerPage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return StickerState();
   }
-
 }
 
 class StickerState extends State<StickerPage> {
-  var images = ['images/emoge_one.png',
-  'images/emoge_two.jpg',
-  'images/emoge_three.png',
-  'images/emoge_four.png',
-  'images/emoge_five.png'
+  var images = [
+    'images/emoge_one.png',
+    'images/emoge_two.jpg',
+    'images/emoge_three.png',
+    'images/emoge_four.png',
+    'images/emoge_five.png'
   ];
 
   double _zoom;
@@ -32,11 +25,7 @@ class StickerState extends State<StickerPage> {
   Offset _position;
   String _img;
   List<Widget> _widgets;
-  List<AnimModel> _model;
-  int pos=0;
-
-
-
+  int pos = 0;
 
   @override
   void initState() {
@@ -44,11 +33,9 @@ class StickerState extends State<StickerPage> {
     _previousZoom = null;
     _offset = Offset.zero;
     _position = Offset(0.0, 0.0);
-    _widgets=List();
-    _model=List();
+    _widgets = List();
     super.initState();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -56,35 +43,34 @@ class StickerState extends State<StickerPage> {
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
-
         children: <Widget>[
-        Positioned(
-        bottom: 0.0,
-        right: 0.0,
-        child: FloatingActionButton(
-          onPressed: () {
-            showEmoges(context);
-          },
-          child: Icon(Icons.add),
-        ),
-      ),
-
-
-  Stack(
-     children: _widgets,
-
-      ),
+          Positioned(
+            bottom: 0.0,
+            right: 0.0,
+            child: FloatingActionButton(
+              onPressed: () {
+                showEmoges(context);
+              },
+              child: Icon(Icons.add),
+            ),
+          ),
+          Stack(
+            children: _widgets,
+          ),
         ],
       ),
     );
   }
 
   void showEmoges(context) {
-    showModalBottomSheet(context: context, builder: (BuildContext) {
-      return GridView.count(crossAxisCount: 3,
-        children: List.generate(images.length, (index) {
-          return Center(
-              child: InkWell(
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext) {
+          return GridView.count(
+            crossAxisCount: 3,
+            children: List.generate(images.length, (index) {
+              return Center(
+                  child: InkWell(
                 onTap: () {
                   addImg(images[index]);
                 },
@@ -92,15 +78,13 @@ class StickerState extends State<StickerPage> {
                   padding: const EdgeInsets.all(8.0),
                   child: Image.asset(images[index]),
                 ),
-              )
+              ));
+            }),
           );
-        }),
-      );
-    });
+        });
   }
 
   void _handleScaleStart(ScaleStartDetails start) {
-
     setState(() {
       _previousOffset = _offset;
       _previousZoom = _zoom;
@@ -113,16 +97,18 @@ class StickerState extends State<StickerPage> {
     });
   }
 
-  _getWidget(img){
-    return  Positioned(
-      left :_position.dx,//horizontal
+  _getWidget(img) {
+    return Positioned(
+      left: _position.dx, //horizontal
       top: _position.dy,
-      child:Draggable( //drag and drop
+      child: Draggable(
+        //drag and drop
         child: new Container(
           padding: const EdgeInsets.all(10.0),
           width: 350.0,
           height: 450.0,
-          child:  GestureDetector( //zoom
+          child: GestureDetector(
+            //zoom
             onScaleStart: _handleScaleStart,
             onScaleUpdate: _handleScaleUpdate,
             child: new Transform(
@@ -131,17 +117,18 @@ class StickerState extends State<StickerPage> {
               alignment: FractionalOffset.center,
               child: new Image.asset(img),
             ),
-          ) ,
+          ),
         ),
 
-        onDraggableCanceled: (velocity,
-            offset) { //When you stop moving the image, it is necessary to setState the new coordinates
+        onDraggableCanceled: (velocity, offset) {
+          //When you stop moving the image, it is necessary to setState the new coordinates
           setState(() {
 //        _position = offset;
-            _position=offset;
+            _position = offset;
           });
         },
-        feedback: Container( //Response when moving the image. Increase the width and height to 100.0 to see the difference
+        feedback: Container(
+          //Response when moving the image. Increase the width and height to 100.0 to see the difference
           width: 1.0,
           height: 1.0,
           child: new Image.asset(img),
@@ -150,13 +137,12 @@ class StickerState extends State<StickerPage> {
     );
   }
 
-
   void addImg(img) {
-   setState(() {
-     _widgets.add(_getWidget(img));
-   });
+    setState(() {
+      _widgets.add(_getWidget(img));
+    });
 
-   /* setState(() {
+    /* setState(() {
       _img=img;
       AnimModel animModel=AnimModel();
       animModel.position=Offset(0.2, 0.4);
@@ -195,7 +181,7 @@ class StickerState extends State<StickerPage> {
   );
   _model.add(animModel);*/
 
-      /*_widgets.add(
+    /*_widgets.add(
            Draggable( //drag and drop
               child: new Container(
                 padding: const EdgeInsets.all(10.0),
@@ -231,8 +217,4 @@ class StickerState extends State<StickerPage> {
     */
     Navigator.pop(context);
   }
-
-
-
-
 }
